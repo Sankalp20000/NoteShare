d39b6986487b21824d5895985fff7bf5d17ecd64
Sankalp <rana.sankalp99@gmail.com>
2023-08-07 17:37:11 +0530

    add cors

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express  = require('express'); // express is a popular library used to build web server
 const app = express(); // yarn is a package manager, just like npm 
 const path = require('path');
+const cors = require('cors');
 
 const PORT = process.env.PORT || 3000;
 app.use(express.static('public'));
@@ -8,6 +9,12 @@ app.use(express.json());
 
 const connectDB = require('./config/db.js');
 connectDB();
+
+// Cors
+const corsOptions = {
+    origin: process.env.ALLOWED_CLIENTS.split(',')
+}
+app.use(cors(corsOptions));
 
 // Template engine
 app.set('views', path.join(__dirname, '/views'));
