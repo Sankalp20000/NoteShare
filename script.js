08fc5069253e18672968f52421d52091eb049003
Sankalp <rana.sankalp99@gmail.com>
2023-08-07 20:50:50 +0530

    Add timeout handling for database query in deleteData function

--- script.js
+++ script.js
@@ -5,6 +5,12 @@ connectDB();
 
 async function deleteData() {
     const pastDate = new Date(Date.now() - 24 * 60 * 60 * 1000); // subtract 24 hours from current time(in ms)
+    // Add a timeout to the database query
+    const timeout = setTimeout(() => {
+        console.log('No files older than pastDate found. Exiting...');
+        process.exit(0);
+    }, 10000); // Timeout after 10 seconds 
+    
     const files = await File.find({ createdAt: {$lt: pastDate}});
     if(files.length) {
         for(const file of files) {
