8e9b8f82d92216dde88c7e10feb554acfb151b65
Sankalp <rana.sankalp99@gmail.com>
2023-08-07 21:36:58 +0530

    Enhancements: Preheader Text and Informative Log Message

--- script.js
+++ script.js
@@ -5,12 +5,7 @@ connectDB();
 
 async function deleteData() {
     const pastDate = new Date(Date.now() - 24 * 60 * 60 * 1000); // subtract 24 hours from current time(in ms)
-    // Add a timeout to the database query
-    const timeout = setTimeout(() => {
-        console.log('No files older than pastDate found. Exiting...');
-        process.exit(0);
-    }, 10000); // Timeout after 10 seconds 
-    
+
     const files = await File.find({ createdAt: {$lt: pastDate}});
     if(files.length) {
         for(const file of files) {
@@ -19,10 +14,13 @@ async function deleteData() {
                 await file.remove();
                 console.log(`successfully deleted ${file.filename}`)
             } catch(err) {
-                console.log(`error while deleteing file ${err}`)
+                console.log(`error while deleting file ${err}`)
             }
         }
         console.log('Job done!');
+    }
+    else{
+        console.log('No files older than 24 hours found. Exiting...');
     }
 }
 
